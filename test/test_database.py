6da3521585bdb9cfd955cda9e6da0ee7a5c6b26c
Clint Daniels <clint.daniels@sandag.org>
2016-02-22 16:32:48 -0800

    clean up dependencies in build.

--- test/test_database.py
+++ test/test_database.py
@@ -1,12 +1,13 @@
 import unittest
-
+import os
 from pysandag import database
 
 
 class TestDatabaseMethods(unittest.TestCase):
     def test_get_connection_string(self):
+        print os.getcwd()
         compare_string = 'mssql+pyodbc://user_name:a_password@sqlserverdatabase/a_database_name?driver=SQL+Server+Native+Client+11.0'
-        self.assertEqual(database.get_connection_string('../db.yml', 'in_db'), compare_string)
+        self.assertEqual(database.get_connection_string('db.yml', 'in_db'), compare_string)
 
 
 if __name__ == '__main__':
